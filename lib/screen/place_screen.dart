4a1d37511889f79c07f7d8d9fc267f51b3ea1e2a
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-03-29 11:31:57 +0600

    set up main file with theme and add screens

---
+++ lib/screen/place_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PlaceScreen extends StatelessWidget {
+  const PlaceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Place'),
+      ),
+    );
+  }
+}
